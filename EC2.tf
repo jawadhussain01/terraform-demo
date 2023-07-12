290e5aeac5c2e1f6e7c3940b1aaf3c868fdd41a7
jawadhussain01 <103805309+jawadhussain01@users.noreply.github.com>
2023-07-13 01:55:21 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIA3W24C6AWTFQOJWWL"
-  secret_key = "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"
 }
 resource "aws_instance" "demoEC2" {
   ami = "ami-06ca3ca175f37dd66"
