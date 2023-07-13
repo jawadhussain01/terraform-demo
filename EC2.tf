d483c71060586c0afce0ba13f528c1c5c67f74e5
jawadhussain01 <103805309+jawadhussain01@users.noreply.github.com>
2023-07-13 08:06:00 +0530

    formatted ec2.tf

--- EC2.tf
+++ EC2.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "demoEC2" {
-  ami = "ami-06ca3ca175f37dd66"
+  ami           = "ami-06ca3ca175f37dd66"
   instance_type = "t2.micro"
 }
