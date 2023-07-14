965b3ececb2c5817d62dd350218cf710c8d3bc9f
jawad <hussain.jm@gmail.com>
2023-07-14 09:21:18 +0530

    test

--- EC2.tf
+++ EC2.tf
@@ -1,7 +1,45 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_security_group" "demoSG" {
+  name        = "allow_http"
+  description = "Allow inbound traffic to ingress"
+  ingress {
+    description = "Allow nginx"
+    from_port   = 0
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_nginx"
+  }
+}
+
 resource "aws_instance" "demoEC2" {
   ami           = "ami-06ca3ca175f37dd66"
   instance_type = "t2.micro"
+  aws_key       = "terraform-windows-key"
+  vpc_security_group_ids = [aws_security_group.demoSG.id]
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      host        = self.public_ip
+      private_key = file("./terraform-key-windows.pem")
+    }
+    inline = ["sudo yum install -y nginx",
+      "sudo systemctl restart nginx"
+    ]
+  }
 }
+
